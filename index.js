686953ab204c686318c4ec240dba1b3fedbebd84
mmanczak <mikomanczak@gmail.com>
2023-06-10 12:31:52 +0200

    feat: added slider with hotels

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const mySiema = new Siema({
+        selector: ".slider",
+        easing: 'ease-out',
+        loop: true
+    });
+  });
