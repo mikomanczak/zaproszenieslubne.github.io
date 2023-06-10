6d8b8d66a1a4be6ab17c6a653b9d49f88a014559
mmanczak <mikomanczak@gmail.com>
2023-06-10 12:59:22 +0200

    fix: updated slider

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const mySiema = new Siema({
         selector: ".slider",
+        duration: 800,
         easing: 'ease-out',
         loop: true
     });
